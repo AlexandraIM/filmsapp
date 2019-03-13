a5523903cb66515b781e12c719c3e3c0c58a3586
AlexandraIM <alexandra.imereli@mustardlabs.com>
2019-03-13 22:30:11 +0200

    Updates

---
+++ precache-manifest.8fbb5a23dfb7dbaab0d514b2590e8c82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c9aaee2759fe23c65c5",
+    "url": "/filmsapp/static/js/runtime~main.7c1e4107.js"
+  },
+  {
+    "revision": "6746271c3cd1b720d3d6",
+    "url": "/filmsapp/static/js/main.db06fcd6.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/filmsapp/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "6746271c3cd1b720d3d6",
+    "url": "/filmsapp/static/css/main.3034a115.chunk.css"
+  },
+  {
+    "revision": "68e40947c02cf8b0e4e31f236e58b994",
+    "url": "/filmsapp/index.html"
+  }
+];
