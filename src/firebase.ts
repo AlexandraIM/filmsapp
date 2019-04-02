993ad5b60a0ce4e8eb66a3c9691187cfd39e632a
Alexandra <alexandra.imereli@mustardlabs.com>
2019-04-02 12:46:41 +0300

    -some changes

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,5 @@
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/firebase-database';
 
 let config = {
   apiKey: "AIzaSyAeYNRj5191QwKNJbWMVvakRASTsT_YbFQ",
